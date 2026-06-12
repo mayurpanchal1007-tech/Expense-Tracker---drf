dc39a1b32411f9290e3fdb4eaa79ae236011af34
mayurpanchal1007-tech <mayurpanchal1007@gmail.com>
2026-06-12 17:20:56 +0530

    serializers done

--- expenseapp/admin.py
+++ expenseapp/admin.py
@@ -1,3 +1,9 @@
+from .models import Expense
 from django.contrib import admin
 
-# Register your models here.
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ("id", "amount", "description", "date")
+    search_fields = ("description",)
+    list_filter = ("date",)
+    
+admin.site.register(Expense, ExpenseAdmin)
